e11987e8cddbf364b5e184204899ec140f22221e
Omar Damak <omardamak1@gmail.com>
2020-01-03 16:12:14 +0100

    First commit

---
+++ src/main/java/com/luv2code/aopdemo/MainController.java
@@ -0,0 +1,36 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+
+	@GetMapping("/test")
+	public String MainTest() {
+		
+		System.out.println("hello from /main/test");
+		
+		// read spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		// call the business method
+		theAccountDAO.addAccount();
+		
+		// close the context
+		context.close();
+
+		return "main-test";
+	}
+	
+
+}
+
---
+++ src/main/webapp/WEB-INF/view/main-test.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World from main-test!</h2>
+</body>
+</html>
