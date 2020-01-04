4e09585d4d3f70cf24d8dbed899a36f18343efe8
Omar Damak <omardamak1@gmail.com>
2020-01-04 07:55:36 +0100

    Match method with any return type

--- src/main/java/com/luv2code/aopdemo/dao/FriendDAO.java
+++ src/main/java/com/luv2code/aopdemo/dao/FriendDAO.java
@@ -5,9 +5,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class FriendDAO {
 	
-public void addFriend() {
+public String addFriend() {
 		
 		System.out.println(getClass() + ": DOING MY DB WORK: ADDING A FRIEND");
+		return "I am the return String of FriendDAO.addFriend";
 		
 	}
 
